5a0e57e20ee0b6be8b0c99929e645e6783803ec2
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2020-11-26 03:49:40 -1000

    commit (#20)

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export type TypeofMap = {
+  string: string,
+  number: number,
+  boolean: boolean
+}
+export type PrimitivTypeString = keyof TypeofMap;
+
+export type PrimitivType = string | number | boolean;
+
+export type EnvNameServer = 'FAUCET_ACCOUNT_MNEMONIC'|
+ 'INJECTED_TYPES'|
+ 'NETWORK_DECIMALS'|
+ 'PORT' |
+ 'RPC_ENDPOINT'
+ ;
+
+export type EnvNameBot = 'BACKEND_URL'|
+ 'DRIP_AMOUNT' |
+ 'MATRIX_ACCESS_TOKEN'|
+ 'MATRIX_BOT_USER_ID'|
+ 'NETWORK_DECIMALS'|
+ 'NETWORK_UNIT'
+;
+
+export interface EnvOpt {
+  default?: PrimitivType;
+  required: boolean;
+  secret: boolean;
+  type: PrimitivTypeString;
+}
+
+export type EnvVar<T extends EnvNameServer | EnvNameBot> = Record<T, EnvOpt>;
