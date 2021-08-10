9a811db5cead2ea21ac348c0f17d7079d67574d6
Wilfried Kopp <wilfried@parity.io>
2021-08-10 14:30:14 +0200

    Add decoding of the address

--- src/bot/ignorellist.ts
+++ src/bot/ignorellist.ts
@@ -1,4 +1,15 @@
-export function isIgnored (account: string): boolean {
+import { decodeAddress } from '@polkadot/keyring';
+import { logger } from 'src/utils';
+
+export function isIgnored(account: string): boolean {
+  let decodedAddress: string;
+  try {
+    decodedAddress = decodeAddress(account).toString();
+  } catch (e) {
+    logger.warn(`${account} is not in the proper format.`);
+    return false;
+  }
   const ignoreList: string[] = (process.env.FAUCET_IGNORE_LIST ?? '').replace(/\s/g, '').split(',') ?? [];
-  return ignoreList.includes(account);
+
+  return ignoreList.includes(decodedAddress);
 }
