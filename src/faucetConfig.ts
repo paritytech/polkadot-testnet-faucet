e5d01640a47dbf1c9362f0bd77d42e59d95cfca3
rzadp <roopert7@gmail.com>
2023-01-16 18:42:09 +0100

    Shorten the implementation

--- src/faucetConfig.ts
+++ src/faucetConfig.ts
@@ -9,17 +9,15 @@ type SpecType<T> = T extends { type: "string" } ? string : T extends { type: "nu
 export function faucetBotConfig() {
   const config = faucetConfig("bot");
   type BotConfigSpec = typeof botConfigSpec["SMF"]["BOT"];
-  type BotConfigKey = keyof BotConfigSpec;
-  type Ret<K extends BotConfigKey> = SpecType<BotConfigSpec[K]>;
-  return { Get: <K extends BotConfigKey>(key: K): Ret<K> => config.Get("BOT", key) };
+  return { Get: <K extends keyof BotConfigSpec>(key: K): SpecType<BotConfigSpec[K]> => config.Get("BOT", key) };
 }
 
 export function faucetServerConfig() {
   const config = faucetConfig("server");
   type ServerConfigSpec = typeof serverConfigSpec["SMF"]["BACKEND"];
-  type ServerConfigKey = keyof ServerConfigSpec;
-  type Ret<K extends ServerConfigKey> = SpecType<ServerConfigSpec[K]>;
-  return { Get: <K extends ServerConfigKey>(key: K): Ret<K> => config.Get("BACKEND", key) };
+  return {
+    Get: <K extends keyof ServerConfigSpec>(key: K): SpecType<ServerConfigSpec[K]> => config.Get("BACKEND", key),
+  };
 }
 
 function faucetConfig(appName: "server" | "bot") {
