3b776c0e618d01694ff6ec23554625a51830efa9
rzadp <roopert7@gmail.com>
2023-01-16 18:29:07 +0100

    Minimize diff

--- src/faucetConfig.ts
+++ src/faucetConfig.ts
@@ -1,36 +1,30 @@
-import { ConfigManager, ConfigObject } from "confmgr/lib";
+import { ConfigManager } from "confmgr/lib";
 
 import botConfigSpec from "../env.bot.config.json";
 import serverConfigSpec from "../env.server.config.json";
 import { logger } from "./logger";
 
+type SpecType<T> = T extends { type: "string" } ? string : T extends { type: "number" } ? number : never;
+
 export function faucetBotConfig() {
-  const config = ConfigManager.getInstance("env.bot.config.json").getConfig();
-  validateConfig(config, "bot");
+  const config = faucetConfig("bot");
   type BotConfigSpec = typeof botConfigSpec["SMF"]["BOT"];
   type BotConfigKey = keyof BotConfigSpec;
-  type Ret<K extends BotConfigKey> = BotConfigSpec[K] extends { type: "string" }
-    ? string
-    : BotConfigSpec[K] extends { type: "number" }
-    ? number
-    : never;
+  type Ret<K extends BotConfigKey> = SpecType<BotConfigSpec[K]>;
   return { Get: <K extends BotConfigKey>(key: K): Ret<K> => config.Get("BOT", key) };
 }
 
 export function faucetServerConfig() {
-  const config = ConfigManager.getInstance("env.server.config.json").getConfig();
-  validateConfig(config, "server");
+  const config = faucetConfig("server");
   type ServerConfigSpec = typeof serverConfigSpec["SMF"]["BACKEND"];
   type ServerConfigKey = keyof ServerConfigSpec;
-  type Ret<K extends ServerConfigKey> = ServerConfigSpec[K] extends { type: "string" }
-    ? string
-    : ServerConfigSpec[K] extends { type: "number" }
-    ? number
-    : never;
+  type Ret<K extends ServerConfigKey> = SpecType<ServerConfigSpec[K]>;
   return { Get: <K extends ServerConfigKey>(key: K): Ret<K> => config.Get("BACKEND", key) };
 }
 
-function validateConfig(config: ConfigObject, appName: "server" | "bot") {
+function faucetConfig(appName: "server" | "bot") {
+  const config = ConfigManager.getInstance(`env.${appName}.config.yml`).getConfig();
+
   if (process.env.NODE_ENV !== "test") {
     config.Print({ compact: true });
 
@@ -40,4 +34,6 @@ function validateConfig(config: ConfigObject, appName: "server" | "bot") {
       logger.info(`✅ ${appName} config validated`);
     }
   }
+
+  return config;
 }
