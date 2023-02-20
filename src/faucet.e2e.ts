24dd683b46c8862ad69401cbb2c2cdc982043f81
rzadp <roopert7@gmail.com>
2023-02-20 11:15:32 +0100

    WIP steps from last week

--- src/faucet.e2e.ts
+++ src/faucet.e2e.ts
@@ -1,23 +1,38 @@
 import { ApiPromise } from "@polkadot/api";
 import { HttpProvider } from "@polkadot/rpc-provider";
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { until } from "opstooling-js";
+import { GenericContainer, DockerComposeEnvironment } from "testcontainers";
+import path from "path";
+import { Wait } from "testcontainers";
 
 describe("Faucet E2E", () => {
   const userAddress = "1useDmpdQRgaCmkmLFihuw1Q4tXTfNKaeJ6iPaMLcyqdkoS"; // Random address.
-  const matrix = axios.create({ baseURL: "http://localhost:8008" });
+  let polkadotApi: ApiPromise
+  let matrix: AxiosInstance
   let roomId: string;
   let userAccessToken: string;
-
-  const polkadotApi = new ApiPromise({
-    provider: new HttpProvider("http://localhost:9933"),
-    types: { Address: "AccountId", LookupSource: "AccountId" },
-  });
+  let adminAccessToken: string;
+  let botAccessToken: string;
 
   const login = async (user: string, password: string): Promise<string> => {
     const result = await matrix.post("_matrix/client/v3/login", { type: "m.login.password", user, password });
     return result.data.access_token;
   };
+
+  const prepareRoom = async (): Promise<string> => {
+    const result = await matrix.post(`/_matrix/client/v3/createRoom??access_token=${adminAccessToken}`, {
+      "room_alias_name": "faucet"
+    });
+    const roomId = result.data.room_id
+    await matrix.post(`/_matrix/client/v3/rooms/${roomId}/invite?access_token=${adminAccessToken}`, {
+      "user_id": "@bot:localhost"
+    });
+    await matrix.post(`/_matrix/client/v3/rooms/${roomId}/invite?access_token=${adminAccessToken}`, {
+      "user_id": "@user:localhost"
+    });
+    return roomId
+  }
 
   const postMessage = async (body: string) => {
     await matrix.post(`/_matrix/client/v3/rooms/${roomId}/send/m.room.message?access_token=${userAccessToken}`, {
@@ -40,32 +55,72 @@ describe("Faucet E2E", () => {
   };
 
   beforeAll(async () => {
-    userAccessToken = await login("user", "user");
-    await polkadotApi.isReady;
-    /*
-    We should have already joined the room, but we repeat it to retrieve the room id.
-    We cannot send a message to a room via alias.
-    */
-    const room = await matrix.post(`/_matrix/client/v3/join/%23faucet:localhost?access_token=${userAccessToken}`, {});
-    roomId = room.data.room_id;
-  });
+    console.log("A")
+    const composeFilePath = path.resolve(__dirname, "../e2e");
 
-  test("The bot responds to the !balance message", async () => {
-    await postMessage("!balance");
 
-    await until(async () => (await getLatestMessage()).sender === "@bot:localhost", 500, 10, "Bot did not reply.");
-    const botMessage = await getLatestMessage();
-    expect(botMessage.body).toEqual("The faucet has 10000 UNITs remaining.");
-  });
 
-  test("The bots drips to a given address", async () => {
-    expect((await getUserBalance()).eqn(0)).toBeTruthy();
+    const environment = await new DockerComposeEnvironment(composeFilePath, "docker-compose.yml")
+      .withWaitStrategy("e2e-polkadot", Wait.forLogMessage("Running JSON-RPC HTTP server"))
+      .withWaitStrategy("e2e-matrix", Wait.forLogMessage("Setting up server"))
+      .up();
 
-    await postMessage(`!drip ${userAddress}`);
 
-    await until(async () => (await getLatestMessage()).sender === "@bot:localhost", 500, 10, "Bot did not reply.");
-    const botMessage = await getLatestMessage();
-    expect(botMessage.body).toContain("Sent @user:localhost 10 UNITs.");
-    await until(async () => (await getUserBalance()).gtn(0), 500, 15, "balance did not increase.");
-  });
+    const matrixContainer = environment.getContainer("e2e-matrix")
+    const polkadotContainer = environment.getContainer("e2e-polkadot")
+
+    await matrixContainer.exec(["e2e-matrix register_new_matrix_user --user admin --password admin -c /data/homeserver.yaml --admin"])
+    await matrixContainer.exec(["e2e-matrix register_new_matrix_user --user bot --password bot -c /data/homeserver.yaml --admin"])
+    await matrixContainer.exec(["e2e-matrix register_new_matrix_user --user user --password user -c /data/homeserver.yaml --admin"])
+    matrix = axios.create({ baseURL: `http://localhost:${matrixContainer.getMappedPort(8008)}` });
+
+    polkadotApi = new ApiPromise({
+      provider: new HttpProvider(`http://localhost:${polkadotContainer.getMappedPort(9933)}`),
+      types: { Address: "AccountId", LookupSource: "AccountId" },
+    });
+
+    const botImage = await GenericContainer.fromDockerfile(path.resolve(__dirname, ".."), "bot_injected.Dockerfile")
+      .build();
+
+    const botContainer = await botImage
+      .start();
+
+    const serverImage = await GenericContainer.fromDockerfile(path.resolve(__dirname, ".."), "server_injected.Dockerfile")
+      .build();
+
+    const serverContainer = await serverImage
+      .start();
+  }, 60_000)
+
+  // beforeAll(async () => {
+  //   adminAccessToken = await login("admin", "admin")
+  //   botAccessToken = await login("bot", "bot")
+  //   userAccessToken = await login("user", "user");
+  //   roomId = await prepareRoom()
+  // })
+  //
+  // beforeAll(async () => await polkadotApi.isReady);
+
+  test("testcontainers", async () => {
+
+  })
+
+  // test("The bot responds to the !balance message", async () => {
+  //   await postMessage("!balance");
+  //
+  //   await until(async () => (await getLatestMessage()).sender === "@bot:localhost", 500, 10, "Bot did not reply.");
+  //   const botMessage = await getLatestMessage();
+  //   expect(botMessage.body).toEqual("The faucet has 10000 UNITs remaining.");
+  // });
+  //
+  // test("The bots drips to a given address", async () => {
+  //   expect((await getUserBalance()).eqn(0)).toBeTruthy();
+  //
+  //   await postMessage(`!drip ${userAddress}`);
+  //
+  //   await until(async () => (await getLatestMessage()).sender === "@bot:localhost", 500, 10, "Bot did not reply.");
+  //   const botMessage = await getLatestMessage();
+  //   expect(botMessage.body).toContain("Sent @user:localhost 10 UNITs.");
+  //   await until(async () => (await getUserBalance()).gtn(0), 500, 15, "balance did not increase.");
+  // });
 });
