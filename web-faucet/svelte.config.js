8d40bfaee9d69fb03cf68ca2163c72d6d60982ee
Javier Bullrich <javier@bullrich.dev>
2023-02-20 16:44:14 +0100

    feat: added daisyUI

--- web-faucet/svelte.config.js
+++ web-faucet/svelte.config.js
@@ -1,7 +1,10 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import preprocess from 'svelte-preprocess'
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 }
+
