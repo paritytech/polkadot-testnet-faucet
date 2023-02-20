2333b446cab4b922d7b3fa006f70c09c60fef0e4
Javier Bullrich <javier@bullrich.dev>
2023-02-20 17:23:24 +0100

    added recaptcha v3

--- web-faucet/src/lib/Form.svelte
+++ web-faucet/src/lib/Form.svelte
@@ -3,23 +3,42 @@
   import polkadot from "../assets/polkadot.png";
   import Cross from "./icons/Cross.svelte";
 
+  const captchaKey: string = import.meta.env.VITE_CAPTCHA_KEY;
+
   let address: string = '';
 
   let webRequest: Promise<boolean> = null;
 
   function onSubmit() {
-    console.log("YES!");
     webRequest = boilerplateRequest(address);
+  }
+
+  function doRecaptcha() {
+    return new Promise<string>((res, rej) => {
+      // @ts-ignore
+      const captcha: Captcha = grecaptcha;
+      captcha.ready(function () {
+        captcha.execute(captchaKey, {action: "submit"}).then(function (t) {
+          return res(t);
+        }).catch(rej);
+      });
+    });
   }
 
   async function boilerplateRequest(address: string) {
     await new Promise((resolve) => setTimeout(resolve, 1000));
+    const token = await doRecaptcha();
     if (address === "error") {
       throw new Error("This is a terrible error!");
     }
+    console.log(token);
     return true;
   }
 </script>
+
+<svelte:head>
+  <script src="https://www.google.com/recaptcha/api.js?render={captchaKey}" async defer></script>
+</svelte:head>
 
 <div class="card md:w-2/4 w-5/6 bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
