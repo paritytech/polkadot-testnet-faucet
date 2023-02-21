d8a080524f6159f45c8d66e84185f6e620331606
Javier Bullrich <javier@bullrich.dev>
2023-02-21 15:09:51 +0100

    cleaned up the captcha component

--- client/src/lib/CaptchaV2.svelte
+++ client/src/lib/CaptchaV2.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import {createEventDispatcher} from "svelte";
 
-  const dispatch = createEventDispatcher<{ onToken: string, onExpire: string }>();
+  const dispatch = createEventDispatcher<{ token: string }>();
 
   const captchaId = 'captcha_element';
 
   window.captchaLoaded = () => {
     const darkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const mobileScreen = window.innerHeight > window.innerWidth;
+
     // @ts-ignore
     const captcha: Captcha = grecaptcha;
 
@@ -14,16 +16,18 @@
       sitekey: '6Ldlmp8kAAAAACEOROGslrJ7gUZjmJIm6WBPUVki',
       theme: darkTheme ? "dark" : "light",
       callback: 'onToken',
+      size: mobileScreen ? 'compact' : 'normal',
       'expired-callback': 'onExpiredToken'
     });
   }
 
   window.onToken = (token) => {
-    dispatch('onToken', token);
+    dispatch('token', token);
   }
 
+  // clean the token so the form becomes invalid
   window.onExpiredToken = () => {
-    dispatch('onToken', '');
+    dispatch('token', '');
   }
 </script>
 
