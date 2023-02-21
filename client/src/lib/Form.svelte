43169ffa0ee971b117017c75aebe95536c1df21c
Javier Bullrich <javier@bullrich.dev>
2023-02-21 13:46:34 +0100

    added select field with networks

--- client/src/lib/Form.svelte
+++ client/src/lib/Form.svelte
@@ -27,17 +27,34 @@
 
 
 <form on:submit|preventDefault={onSubmit} class="w-full">
-  <label class="label" for="address">
-    <span class="label-text">Your SS58 Address</span>
-  </label>
-  <input
-    type="text"
-    bind:value={address}
-    placeholder="Enter your address"
-    class="input w-full input-primary mb-6 text-sm"
-    id="address"
-    disabled={!!webRequest}
-  />
+  <div class="md:grid md:grid-cols-4 md:gap-4 inputs-container">
+    <div class="col-span-3">
+      <label class="label" for="address">
+        <span class="label-text">Your SS58 Address</span>
+      </label>
+      <input
+        type="text"
+        bind:value={address}
+        placeholder="Enter your address"
+        class="input w-full input-primary text-sm"
+        id="address"
+        disabled={!!webRequest}
+      />
+    </div>
+    <div class="form-control w-full max-w-xs">
+      <label class="label" for="network">
+        <span class="label-text">Network</span>
+      </label>
+      <select class="select select-bordered select-primary" id="network">
+        <option selected>Roccoco</option>
+        <option>Star Wars</option>
+        <option>Harry Potter</option>
+        <option>Lord of the Rings</option>
+        <option>Planet of the Apes</option>
+        <option>Star Trek</option>
+      </select>
+    </div>
+  </div>
   {#if !webRequest}
     <div class="grid place-items-center">
       <CaptchaV2 captchaKey={CAPTCHA_KEY} on:token={onToken}/>
@@ -76,3 +93,9 @@
     {/await}
   {/if}
 </form>
+
+<style>
+  .inputs-container {
+    margin-bottom: 1.5rem;
+  }
+</style>
