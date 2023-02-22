56130249044c67d12fa9a434c13314d032d55d22
Javyer <javier.bullrich@parity.io>
2023-02-22 13:51:58 +0100

    Divided endpoints based on faucet or drip bot (#211)
    This PR tries to divide the endpoints into two different methods.
    
    It adds a couple of improvements to reduce duplicated code.
    
    # Endpoints
    
    It created the following endpoints:
    - `faucet/`
    - `drip-v2/`
    
    I kept the default drip endpoint to not break the already existing
    implementation, but if `drip-v2` works, we could replace `drip` with
    that one.
    
    The `faucet` endpoint only handles the expected values coming from the
    faucet clients (!ink docs and the web faucet) while the drip endpoint
    handles the other part of the requests.
    
    This way we remove ifs from the code and can also isolate the endpoints.
    
    It adds a couple of improvements to reduce duplicated code.
    
    ## Middleware
    
    ```typescript
    const addressMiddleware = (
      req: Request<unknown, DripResponse, Partial<DripRequestType>>,
      res: Response,
      next: NextFunction,
    ): void => {
      if (!req.body.address) {
        return missingParameterError(res, "address");
      }
      next();
    };
    ```
    
    It creates this middleware to ensure that the field address is not
    empty.
    
    ## Status code in errors
    
    As the current implementation doesn't return an error status code, I add
    them so the system can show an error on the request. I also added HTTP
    codes based on the type of errors.
    
    ```typescript
    res.status(400).send({ error: "Operation failed." });
    ```
    
    ## Missing field error
    
    I simplified the code of a missing variable into a method that is void
    (so it can be returned too)
    ```typescript
    const missingParameterError = (res: Response<DripErrorResponse>, parameter: string): void => {
      res.status(400).send({ error: `Missing parameter: '${parameter}'` });
    };
    ```
    
    ---------
    
    Co-authored-by: Przemek Rzad <przemek@parity.io>

--- src/server/routes/actions.ts
+++ src/server/routes/actions.ts
@@ -1,8 +1,15 @@
 import cors from "cors";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 import { logger } from "../../logger";
-import { BalanceResponse, DripRequestType, DripResponse } from "../../types";
+import {
+  BalanceResponse,
+  BotRequestType,
+  DripErrorResponse,
+  DripRequestType,
+  DripResponse,
+  FaucetRequestType,
+} from "../../types";
 import { config } from "../config";
 import actions from "../services/Actions";
 import ActionStorage from "../services/ActionStorage";
@@ -26,19 +33,92 @@ router.get<unknown, BalanceResponse>("/balance", (_, res) => {
     });
 });
 
+const missingParameterError = (
+  res: Response<DripErrorResponse>,
+  parameter: keyof BotRequestType | keyof FaucetRequestType,
+): void => {
+  res.status(400).send({ error: `Missing parameter: '${parameter}'` });
+};
+
+const addressMiddleware = (
+  req: Request<unknown, DripResponse, Partial<DripRequestType>>,
+  res: Response,
+  next: NextFunction,
+): void => {
+  if (!req.body.address) {
+    return missingParameterError(res, "address");
+  }
+  next();
+};
+
+type PartialDrip<T extends FaucetRequestType | BotRequestType> = Partial<T> & Pick<T, "address">;
+
 const dripRequestHandler = new DripRequestHandler(actions, storage, recaptchaService);
+
+router.post<unknown, DripResponse, PartialDrip<FaucetRequestType>>("/drip/web", addressMiddleware, async (req, res) => {
+  const { address, parachain_id, recaptcha } = req.body;
+  if (!recaptcha) {
+    return missingParameterError(res, "recaptcha");
+  }
+  try {
+    const dripResult = await dripRequestHandler.handleRequest({
+      external: true,
+      address,
+      parachain_id: parachain_id ?? "",
+      amount: config.Get("DRIP_AMOUNT"),
+      recaptcha,
+    });
+
+    if ((dripResult as DripErrorResponse).error) {
+      res.status(500).send(dripResult);
+    } else {
+      res.send(dripResult);
+    }
+  } catch (e) {
+    logger.error(e);
+    errorCounter.plusOne("other");
+    res.status(500).send({ error: "Operation failed." });
+  }
+});
+
+router.post<unknown, DripResponse, PartialDrip<BotRequestType>>("/drip/bot", addressMiddleware, async (req, res) => {
+  // Do not allow this endpoint to be accessed from outside. Should only be for the bot
+  if (config.Get("EXTERNAL_ACCESS")) {
+    return res.status(503).send({ error: "Endpoint unavailable" });
+  }
+  const { address, parachain_id, amount, sender } = req.body;
+  if (!amount) {
+    return missingParameterError(res, "amount");
+  }
+  if (!sender) {
+    return missingParameterError(res, "sender");
+  }
+  try {
+    res.send(
+      await dripRequestHandler.handleRequest({
+        external: false,
+        address,
+        parachain_id: parachain_id ?? "",
+        amount,
+        sender,
+      }),
+    );
+  } catch (e) {
+    logger.error(e);
+    errorCounter.plusOne("other");
+    res.status(500).send({ error: "Operation failed." });
+  }
+});
 
 router.post<unknown, DripResponse, Partial<DripRequestType>>("/drip", async (req, res) => {
   try {
     const { address, parachain_id, amount, sender, recaptcha } = req.body;
     if (!address) {
-      res.send({ error: "Missing parameter: 'address'" });
-      return;
+      return missingParameterError(res, "address");
     }
     if (config.Get("EXTERNAL_ACCESS")) {
       if (!recaptcha) {
-        res.send({ error: "Missing parameter: 'recaptcha'" });
-        return;
+        return missingParameterError(res, "recaptcha");
       }
       res.send(
         await dripRequestHandler.handleRequest({
@@ -51,12 +131,10 @@ router.post<unknown, DripResponse, Partial<DripRequestType>>("/drip", async (req
       );
     } else {
       if (!amount) {
-        res.send({ error: "Missing parameter: 'amount'" });
-        return;
+        return missingParameterError(res, "amount");
       }
       if (!sender) {
-        res.send({ error: "Missing parameter: 'sender'" });
-        return;
+        return missingParameterError(res, "sender");
       }
       res.send(
         await dripRequestHandler.handleRequest({
@@ -71,7 +149,7 @@ router.post<unknown, DripResponse, Partial<DripRequestType>>("/drip", async (req
   } catch (e) {
     logger.error(e);
     errorCounter.plusOne("other");
-    res.send({ error: "Operation failed." });
+    res.status(400).send({ error: "Operation failed." });
   }
 });
 
