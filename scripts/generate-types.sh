048c213f1ccfaff609fc9489e0c7ae1f295026b6
rzadp <roopert7@gmail.com>
2023-02-24 12:25:18 +0100

    First idea about splitting common, bot and web parts of the server variables

--- scripts/generate-types.sh
+++ scripts/generate-types.sh
@@ -2,5 +2,7 @@
 set -euo pipefail
 cd "$(dirname $0)/.."
 
-echo "declare const schema: $(cat env.server.config.json); export default schema;" > env.server.config.json.d.ts
 echo "declare const schema: $(cat env.bot.config.json); export default schema;" > env.bot.config.json.d.ts
+echo "declare const schema: $(cat env.backend.config.json); export default schema;" > env.backend.config.json.d.ts
+echo "declare const schema: $(cat env.bot-backend.config.json); export default schema;" > env.bot-backend.config.json.d.ts
+echo "declare const schema: $(cat env.web-backend.config.json); export default schema;" > env.web-backend.config.json.d.ts
