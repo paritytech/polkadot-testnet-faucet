87ccac94e460e55552d6989d4b5224796da41250
rzadp <roopert7@gmail.com>
2023-03-03 15:30:43 +0100

    Add mock clearing (reset state) to beforeEach

--- src/server/routes/actions.spec.ts
+++ src/server/routes/actions.spec.ts
@@ -56,6 +56,7 @@ const parameterError = (parameter: keyof BotRequestType | keyof FaucetRequestTyp
 
 describe("/drip/web tests", () => {
   beforeEach(() => {
+    jest.clearAllMocks()
     app = express();
     app.use(bodyParser.json());
     const routers = express.Router();
