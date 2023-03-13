a84424972a08c644c23dfa1bfd95d1d472869506
Javier Bullrich <javier@bullrich.dev>
2023-03-13 14:47:27 +0100

    added background spot animation

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,14 +2,16 @@
   import Form from './lib/Form.svelte'
   import Card from "./lib/Card.svelte";
   import SocialTags from "./lib/SocialTags.svelte";
+  import BackgroundSpot from "./lib/BackgroundSpot.svelte";
 
   const urlParams = new URLSearchParams(window.location.search);
   const parachainQuery = urlParams.get('parachain');
   const parachain = parseInt(parachainQuery);
 </script>
-
-<main>
+<main class="overflow-y-hidden">
   <SocialTags/>
+  <BackgroundSpot/>
+
   <h1>Rococo Faucet</h1>
 
   <div class="flex items-center justify-center my-16">
@@ -22,10 +24,13 @@
     Check out <a href="https://use.ink/" target="_blank" rel="noreferrer">Ink!</a>, the official Substrate
     documentation!
   </p>
+
 </main>
 
 <style>
+
   .read-the-docs {
     color: #888;
   }
+
 </style>
---
+++ client/src/lib/BackgroundSpot.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  let blob: HTMLElement;
+
+  function handleMouseMove(event: MouseEvent) {
+    const {clientX, clientY} = event;
+
+    blob.animate({
+      left: `${clientX}px`,
+      top: `${clientY}px`
+    }, {duration: 3000, fill: "forwards"});
+  }
+</script>
+
+<svelte:body on:mousemove={handleMouseMove}/>
+<div bind:this={blob} class="glow-spot invisible md:visible"></div>
+
+<style>
+  .glow-spot {
+    height: 10vmax;
+    aspect-ratio: 1;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    translate: -50% -50%;
+    border-radius: 50%;
+    background: linear-gradient(to right,
+    aquamarine, mediumpurple);
+    animation: rotate 20s infinite;
+    filter: blur(500px);
+    opacity: 0.3;
+  }
+
+  @keyframes rotate {
+    from {
+      rotate: 0deg;
+    }
+
+    to {
+      rotate: 360deg;
+    }
+  }
+</style>
