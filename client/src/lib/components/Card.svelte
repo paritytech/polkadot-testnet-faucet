c19b6cae7403eb1e3e330258668d2df34348c9e4
Javyer <javier.bullrich@parity.io>
2023-03-31 10:27:09 +0200

    updated logo (#252)
    Updated outdated logo for new image.
    
    Benoit & Lucian did this new amazing logo.
    
    I also modified the animation so it grows the picture with a mask. It
    looks more stable now.

--- client/src/lib/components/Card.svelte
+++ client/src/lib/components/Card.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import polkadot from "../../assets/polkadot.png";
+	import polkadot from "../../assets/polkadot.webp";
 	import { PUBLIC_DEMO_MODE } from "$env/static/public";
 </script>
 
 <div class="card md:w-2/4 w-5/6 bg-base-100 shadow-xl">
-	<figure class="px-10 pt-10 transition duration-500 hover:scale-105">
-		<img src={polkadot} alt="Polkadot" class="rounded-xl" />
+	<figure class="rounded-xl">
+		<img src={polkadot} alt="Polkadot" class="rounded-xl transition duration-500 hover:scale-150" />
 	</figure>
 	<div class="card-body items-center text-center">
 		<slot />
