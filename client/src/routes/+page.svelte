9918f1311f21feb66e987fbbed4fafe2db7db935
Javier Bullrich <javier@bullrich.dev>
2023-05-25 15:59:50 +0200

    added faq to main page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -3,10 +3,14 @@
 	import Form from "$lib/components/Form.svelte";
 	import SocialTags from "$lib/components/SocialTags.svelte";
 	import Error from "$lib/components/screens/Error.svelte";
+	import FrequentlyAskedQuestions from "$lib/components/screens/FrequentlyAskedQuestions.svelte";
 	import Success from "$lib/components/screens/Success.svelte";
 	import { operation } from "$lib/utils/stores";
 	import { onMount } from "svelte";
 	import { fly } from "svelte/transition";
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
 
 	let parachain: number;
 	onMount(() => {
@@ -35,6 +39,7 @@
 			{/if}
 		</Card>
 	</div>
+	<FrequentlyAskedQuestions faq={data.faq} />
 </main>
 
 <style lang="postcss">
