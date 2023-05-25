47ac06be768ae58b1182ac5ef72ede466bcc977f
Javyer <javier.bullrich@parity.io>
2023-05-25 08:23:27 +0200

    client: navbar (#271)
    Closes #270 
    
    Created new nav bar and footer for the website.
    
    Added links to create an issue in this repository and to contact the
    parity watercooler matrix channel for questions.

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+	import Footer from "$lib/components/Footer.svelte";
+	import NavBar from "$lib/components/NavBar.svelte";
 	import "../app.css";
 </script>
 
-<slot />
+<div class="w-full">
+	<NavBar />
+	<slot />
+	<Footer />
+</div>
