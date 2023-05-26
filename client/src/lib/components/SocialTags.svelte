839d8a7e34f6624f2470fd6567ac7e93b31932b8
Javier Bullrich <javier@bullrich.dev>
2023-05-26 12:14:31 +0200

    updated seo title

--- client/src/lib/components/SocialTags.svelte
+++ client/src/lib/components/SocialTags.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import { MetaTags } from "svelte-meta-tags";
-	import socialImage from "../../assets/social-image.png";
+	import socialImage from "../../assets/polkadot.png";
 </script>
 
 <MetaTags
-	title="Substrate Faucet"
-	description="A fast and reliable Substrate testnet faucet for blockchain developers."
+	title="Polkadot Faucet"
+	description="A fast and reliable Polkadot testnet faucet for blockchain developers."
 	openGraph={{
 		images: [{ url: socialImage }]
 	}}
 	twitter={{
-		title: "Substrate Faucet",
+		title: "Polkadot Faucet",
 		description: "A fast and reliable Substrate testnet faucet for blockchain developers.",
 		image: socialImage,
-		imageAlt: "Substrate Faucet Website"
+		imageAlt: "Polkadot Faucet Website"
 	}}
 />
