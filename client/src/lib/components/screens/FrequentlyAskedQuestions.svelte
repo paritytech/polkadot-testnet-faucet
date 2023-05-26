141041f43845cc8ef7e94abfe08b5f1e604863c0
Javyer <javier.bullrich@parity.io>
2023-05-26 10:09:26 +0200

    client: added FAQ (#273)
    This commit closes #267 
    
    The new FAQ has been added to the bottom of the page.
    
    It is available as a Markdown file (easy to modify).

---
+++ client/src/lib/components/screens/FrequentlyAskedQuestions.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import SvelteMarkdown from "svelte-markdown";
+	export let faq: string;
+</script>
+
+<div class="flex items-center justify-center my-16">
+	<div class="card md:w-2/4 w-5/6 min-w-full mt-12 md:mt-44">
+		<div class="card-body items-center text-center">
+			<h1>Frequently Asked Questions</h1>
+			<div
+				class="content prose lg:prose-xl prose-headings:text-xl prose-headings:text-white prose-headings:font-bold prose-p:text-opacity-90"
+			>
+				<SvelteMarkdown source={faq} />
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.faucet-card {
+		background-color: #191924;
+	}
+
+	h1 {
+		font-family: "Unbounded", sans-serif;
+		@apply text-white text-4xl;
+		font-weight: 700;
+	}
+
+	.content {
+		@apply mt-2 md:mt-8 w-full text-left;
+		font-family: "Inter", sans-serif;
+		font-weight: 700;
+	}
+</style>
