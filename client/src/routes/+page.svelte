687460587f64fbfd1f41546d2f2a0cc6a41b762e
Javier Bullrich <javier@bullrich.dev>
2023-06-01 16:55:42 +0200

    removed unused components

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,25 +1,6 @@
 <script lang="ts">
 	import Card from "$lib/components/Card.svelte";
-	import Form from "$lib/components/Form.svelte";
 	import SocialTags from "$lib/components/SocialTags.svelte";
-	import Error from "$lib/components/screens/Error.svelte";
-	import FrequentlyAskedQuestions from "$lib/components/screens/FrequentlyAskedQuestions.svelte";
-	import Success from "$lib/components/screens/Success.svelte";
-	import { operation } from "$lib/utils/stores";
-	import { onMount } from "svelte";
-	import { fly } from "svelte/transition";
-	import type { PageData } from "./$types";
-
-	export let data: PageData;
-
-	let parachain: number;
-	onMount(() => {
-		const urlParams = new URLSearchParams(window.location.search);
-
-		const parachainQuery = urlParams.get("parachain") ?? "-1";
-		parachain = parseInt(parachainQuery);
-		console.log(parachain, "parachain");
-	});
 </script>
 
 <main>
