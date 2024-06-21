c60abaec3988faeafd54cbffaa4056061479f5cb
Javier Bullrich <javier@bullrich.dev>
2024-06-21 17:39:21 +0200

    Set paseo as default network
    Resolves #424

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import faqMd from "$lib/assets/FAQ.md?raw";
   import Faucet from "$lib/components/Faucet.svelte";
-  import { type NetworkData, Rococo } from "$lib/utils/networkData";
+  import { type NetworkData, Paseo } from "$lib/utils/networkData";
 
-  let network: NetworkData = Rococo;
+  let network: NetworkData = Paseo;
   let faq: string = faqMd
     .replaceAll("<NETWORK-TOKEN>", network.currency)
     .replaceAll("<NETWORK-NAME>", network.networkName);
