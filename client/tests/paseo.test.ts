3d9bef3717ba5f4efb45b460a8381f0f875bdeb2
Javier Bullrich <javier@bullrich.dev>
2024-10-11 13:28:53 +0200

    updated tests

--- client/tests/paseo.test.ts
+++ client/tests/paseo.test.ts
@@ -1,6 +1,12 @@
 import { FaucetTests } from "./faucet.js";
 
-const chains = [{ name: "Paseo Relay", id: -1 }];
+const chains = [
+  { name: "Paseo Relay", id: -1 },
+  { name: "AssetHub", id: 1000 },
+  { name: "Collectives", id: 1001 },
+  { name: "BridgeHub", id: 1002 },
+  { name: "People", id: 1004 },
+];
 
 const tests = new FaucetTests({ faucetName: "Paseo Faucet", chains, url: "/", expectTransactionLink: false });
 tests.runTests();
