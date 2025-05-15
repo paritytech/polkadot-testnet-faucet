5bf2de385a9939a5344f6059f33158d2b7ea4e2a
cornholio <0@mcornholio.ru>
2025-05-15 19:34:59 +0200

    Westend AH migration

--- client/tests/westend.test.ts
+++ client/tests/westend.test.ts
@@ -1,11 +1,6 @@
 import { FaucetTests } from "./faucet.js";
 
-const chains = [
-  { name: "Westend", id: -1 },
-  { name: "Collectives", id: 1001 },
-  { name: "BridgeHub", id: 1002 },
-  { name: "People", id: 1004 },
-];
+const chains = [{ name: "Westend", id: -1 }];
 
 const tests = new FaucetTests({ faucetName: "Westend Faucet", chains, url: "/westend", expectTransactionLink: true });
 tests.runTests();
