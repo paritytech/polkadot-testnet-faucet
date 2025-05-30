4f2900a5ebd5b1f1e3d9ac05fcf1f58510846b6e
Yuri Volkov <0@mcornholio.ru>
2025-05-30 13:27:26 +0200

    Added Passet Hub (#478)
    Temporary parachain for smart contracts

--- client/src/lib/utils/networkData.ts
+++ client/src/lib/utils/networkData.ts
@@ -38,6 +38,7 @@ export const Paseo: NetworkData = {
   chains: [
     { name: "Paseo Relay", id: -1 },
     { name: "AssetHub", id: 1000 },
+    { name: "Passet Hub: smart contracts", id: 1111 },
     { name: "BridgeHub", id: 1002 },
     { name: "People", id: 1004 },
     { name: "Coretime", id: 1005 },
--- client/tests/paseo.test.ts
+++ client/tests/paseo.test.ts
@@ -3,6 +3,7 @@ import { FaucetTests } from "./faucet.js";
 const chains = [
   { name: "Paseo Relay", id: -1 },
   { name: "AssetHub", id: 1000 },
+  { name: "Passet Hub: smart contracts", id: 1111 },
   { name: "BridgeHub", id: 1002 },
   { name: "People", id: 1004 },
   { name: "Coretime", id: 1005 },
