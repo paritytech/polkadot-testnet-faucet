d8e3e29b12e19db632357267205940f8834733be
Maksym H <mordamax@gmail.com>
2026-02-12 17:06:44 +0000

    updates to labels

--- client/src/lib/utils/networkData.ts
+++ client/src/lib/utils/networkData.ts
@@ -31,7 +31,7 @@ export const Westend: NetworkData = {
   networkName: "Westend",
   currency: "WND",
   chains: [
-    { name: "AssetHub", id: -1 },
+    { name: "Hub (smart contracts)", id: -1 },
     { name: "Westend Relay", id: 0 },
     { name: "Collectives", id: 1001 },
     { name: "BridgeHub", id: 1002 },
@@ -49,9 +49,8 @@ export const Paseo: NetworkData = {
   networkName: "Paseo",
   currency: "PAS",
   chains: [
-    { name: "AssetHub", id: -1 },
+    { name: "Hub (smart contracts)", id: -1 },
     { name: "Paseo Relay", id: 0 },
-    { name: "Passet Hub: smart contracts", id: 1111 },
     { name: "BridgeHub", id: 1002 },
     { name: "People", id: 1004 },
     { name: "Coretime", id: 1005 },
