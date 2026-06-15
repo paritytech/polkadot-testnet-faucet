ca8e7d01323b7e31226c6fd6bb51323aaeb2175e
Maksym H <mordamax@gmail.com>
2026-06-15 10:14:00 +0200

    extract client .papi/ workspace (still PAPI 1.x)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import path from "path";
 import { defineConfig } from "vite";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export default defineConfig({
   plugins: [sveltekit()],
-  resolve: {
-    alias: {
-      "@polkadot-api/descriptors": path.resolve(__dirname, "../.papi/descriptors/dist/index.mjs"),
-    },
-  },
-  server: {
-    fs: {
-      allow: [path.resolve(__dirname, "../.papi/descriptors")],
-    },
-  },
 });
